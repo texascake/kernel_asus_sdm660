2bfe362c31b27bb8ebbf5343a229ec50b23f7b4d
Tiktodz <dotkit@electrowizard.me>
2024-09-09 06:00:10 +0000

    kerneltree: Update builder script
    Change-Id: If78e080f0167222ec0030960595de0493bf1b88a

--- build.sh
+++ build.sh
@@ -1,52 +1,22 @@
 #!/bin/bash
 
+#set -e
 KERNELDIR=$(pwd)
 
 # Identity
+DEVICENAME=X00T/D
 CODENAME=Hayzel
 KERNELNAME=TOM
 VARIANT=HMP
 VERSION=CLO
 
-# The name of the device for which the kernel is built
-MODEL="Asus Max Pro M1"
+COMMIT_BUILD_URL=https://github.com/Tiktodz/android_kernel_asus_sdm636/commits/tom/hmp/
 
-# The codename of the device
-DEVICE="X00TD"
+## Telegram Topic (Default 0 = false)
+TG_SUPER=0
 
-# shellcheck source=/etc/os-release
-DISTRO=$(source /etc/os-release && echo "${NAME}")
-KBUILD_BUILD_HOST=$(uname -a | awk '{print $2}')
-CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-TERM=xterm
-export KBUILD_BUILD_HOST CI_BRANCH TERM
-
-# Specify linker.
-# 'ld.lld'(default)
-LINKER=ld.lld
-
-CHANGELOGS=https://github.com/Tiktodz/android_kernel_asus_sdm636/commits/tom/hmp/
-
-TG_TOPIC=0
+BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
 BOT_BUILD_URL="https://api.telegram.org/bot$TG_TOKEN/sendDocument"
-
-tg_post_build()
-{
-	if [ $TG_TOPIC = 1 ]
-	then
-	    curl -F document=@"$1" "$BOT_BUILD_URL" \
-	    -F chat_id="$TG_CHAT_ID"  \
-	    -F "disable_web_page_preview=true" \
-	    -F "parse_mode=Markdown" \
-	    -F caption="$2"
-	else
-	    curl -F document=@"$1" "$BOT_BUILD_URL" \
-	    -F chat_id="$TG_CHAT_ID"  \
-	    -F "disable_web_page_preview=true" \
-	    -F "parse_mode=Markdown" \
-	    -F caption="$2"
-	fi
-}
 
 tg_post_msg(){
         if [ $TG_SUPER = 1 ]
@@ -66,6 +36,31 @@ tg_post_msg(){
         fi
 }
 
+tg_post_build()
+{
+	if [ $TG_SUPER = 1 ]
+	then
+	    curl -F document=@"$1" "$BOT_BUILD_URL" \
+	    -F chat_id="$TG_CHAT_ID"  \
+	    -F message_thread_id="$TG_TOPIC_ID" \
+	    -F "disable_web_page_preview=true" \
+	    -F "parse_mode=Markdown" \
+	    -F caption="$2"
+	else
+	    curl -F document=@"$1" "$BOT_BUILD_URL" \
+	    -F chat_id="$TG_CHAT_ID"  \
+	    -F "disable_web_page_preview=true" \
+	    -F "parse_mode=Markdown" \
+	    -F caption="$2"
+	fi
+}
+
+tg_post_msg "<b>`date '+%d %b %Y, %H:%M %Z'`</b>
+🔨 Compile kernel <b>$KERNELNAME</b> for <b>$DEVICENAME</b>
+💾 Powered by <b>`source /etc/os-release && echo ${NAME}`</b>
+🆑 Changelog URL <a href='$COMMIT_BUILD_URL'>Click Here</a>
+🙇 Memetainer <b>@queenserenade</b>"
+
 ## Cloning toolchain
 if ! [ -d "$KERNELDIR/ew" ]; then
 mkdir -p $KERNELDIR/ew && cd $KERNELDIR/ew
@@ -77,22 +72,17 @@ fi
 
 ## Copy this script inside the kernel directory
 KERNEL_DEFCONFIG=X00TD_defconfig
-ANYKERNEL3_DIR=$KERNELDIR/AnyKernel3/
-TZ=Asia/Jakarta
 DATE=$(date '+%Y%m%d')
-BUILD_START=$(date +"%s")
-FINAL_KERNEL_ZIP="$KERNELNAME-$VERSION-$VARIANT-$(date '+%Y%m%d-%H%M')"
+FINAL_KERNEL_ZIP="$KERNELNAME-$VARIANT-$VERSION-$(date '+%Y%m%d-%H%M').zip"
 KERVER=$(make kernelversion)
-# Set a commit head
-COMMIT_HEAD=$(git log --oneline -1)
-
-# Exporting
+export KBUILD_BUILD_TIMESTAMP=$(date)
 export PATH="$KERNELDIR/ew/bin:$PATH"
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_BUILD_USER="queen"
 export LLVM=1
 export LLVM_IAS=1
+export KBUILD_BUILD_USER="queen"
+export KBUILD_BUILD_HOST=$(source /etc/os-release && echo "${NAME}" | cut -d" " -f1)
 export KBUILD_COMPILER_STRING="$($KERNELDIR/ew/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 ClangMoreStrings="AR=llvm-ar NM=llvm-nm AS=llvm-as STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf HOSTAR=llvm-ar HOSTAS=llvm-as LD_LIBRARY_PATH=$KERNELDIR/ew/lib LD=ld.lld HOSTLD=ld.lld"
 
@@ -102,35 +92,61 @@ MAKE="./makeparallel"
 # Java
 command -v java > /dev/null 2>&1
 
-# Cleaning out
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
 mkdir -p out
 make O=out clean
 
-# Starting compilation
+echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
+echo -e "$blue***********************************************"
+echo "          BUILDING KERNEL          "
+echo -e "***********************************************$nocol"
 make $KERNEL_DEFCONFIG O=out 2>&1 | tee -a error.log
 make -j$(nproc --all) O=out \
-		ARCH=$ARCH \
-		SUBARCH=$ARCH \
-		CC="$KERNELDIR/ew/bin/clang" \
-		CROSS_COMPILE=aarch64-linux-gnu- \
-		HOSTCC="$KERNELDIR/ew/bin/clang" \
-		HOSTCXX="$KERNELDIR/ew/bin/clang++" ${ClangMoreStrings} 2>&1 | tee -a error.log
+	ARCH=$ARCH \
+	SUBARCH=$ARCH \
+	CC="$KERNELDIR/ew/bin/clang" \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	HOSTCC="$KERNELDIR/ew/bin/clang" \
+	HOSTCXX="$KERNELDIR/ew/bin/clang++" ${ClangMoreStrings} 2>&1 | tee -a error.log
 
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+
+echo "**** Kernel Compilation Completed ****"
+echo "**** Verify Image.gz-dtb ****"
 if ! [ -f $KERNELDIR/out/arch/arm64/boot/Image.gz-dtb ];then
-    tg_post_build "error.log" "Build Error!"
+    tg_post_build "error.log" "Compile Error!!"
+    echo "$red Compile Failed!!!$nocol"
     exit 1
 fi
 
-    tg_post_msg "<b>$KBUILD_BUILD_VERSION Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Linker : </b><code>$LINKER</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<a href='$CHANGELOGS'>Changelogs</a>"
-
 # Anykernel3 time!!
+echo "**** Verifying AnyKernel3 Directory ****"
 if ! [ -d "$KERNELDIR/AnyKernel3" ]; then
-git clone --depth=1 https://github.com/Tiktodz/AnyKernel3 -b hmp-new AnyKernel3
-ls $ANYKERNEL3_DIR
-cp $KERNELDIR/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR
+  echo "AnyKernel3 not found! Cloning..."
+  if ! git clone --depth=1 https://github.com/Tiktodz/AnyKernel3 -b hmp-new AnyKernel3; then
+    tg_post_build "$KERNELDIR/out/arch/arm64/boot/Image.gz-dtb" "Failed to Clone Anykernel, Sending image file instead"
+    echo "Cloning failed! Aborting..."
+    exit 1
+  fi
 fi
 
-# Zipping time!!
+ANYKERNEL3_DIR=$KERNELDIR/AnyKernel3/
+
+# Generating Changelog
+echo "<b><#selectbg_g>$(date)</#></b>" > changelog
+git log --oneline -n15 | cut -d " " -f 2- | awk '{print "<*> " $(A) "</*>"}' | tee -a changelog
+
+echo "**** Copying Image.gz-dtb ****"
+cp $KERNELDIR/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
+
+echo "**** Time to zip up! ****"
 cd $ANYKERNEL3_DIR/
 cp -af $KERNELDIR/init.$CODENAME.Spectrum.rc spectrum/init.spectrum.rc && sed -i "s/persist.spectrum.kernel.*/persist.spectrum.kernel TheOneMemory/g" spectrum/init.spectrum.rc
 cp -af $KERNELDIR/changelog META-INF/com/google/android/aroma/changelog.txt
@@ -162,15 +178,16 @@ cd ../../../..
 
 zip -r9 "../$FINAL_KERNEL_ZIP" * -x .git README.md anykernel-real.sh ./*placeholder .gitignore zipsigner* "*.zip"
 
-ZIP_FINAL="$FINAL_KERNEL_ZIP"
-
 cd ..
 
-curl -sLo zipsigner-3.0.jar https://github.com/Magisk-Modules-Repo/zipsigner/raw/master/bin/zipsigner-3.0-dexed.jar
-java -jar zipsigner-3.0.jar "$ZIP_FINAL".zip "$ZIP_FINAL"-signed.zip
-ZIP_FINAL="$ZIP_FINAL-signed"
-
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
-
-tg_post_build "$ZIP_FINAL.zip" "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)"
+echo "**** Uploading your zip now ****"
+tg_post_build "$FINAL_KERNEL_ZIP" "⏳ *Compile Time*
+• $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds
+🐧 *Linux Version*
+• ${KERVER}
+📀 *Compiler*
+• ${KBUILD_COMPILER_STRING}
+🆕 *Last commit*
+\`\`\`
+$(git log --oneline -n5 | cut -d" " -f2- | awk '{print "• " $(A)}')
+\`\`\`"
